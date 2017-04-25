5e69c5bc5b64c5959e33f4372efa8dc7c1f69e3c
Seth Fitzsimmons <seth@mojodna.net>
2017-04-24 22:13:33 -0700

    Boilerplate++

---
+++ index.android.js
@@ -0,0 +1,80 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import osmdb from 'osm-p2p';
+
+export default class Surveyor extends Component {
+  componentWillMount() {
+    this.osm = osmdb('/tmp/osmdb');
+
+    osm.create({
+      id: 'A',
+      lat: 64.5,
+      lon: -147.6
+    }, (err, key, node) => console.log(err, key, node));
+    osm.create({
+      id: 'B',
+      lat: 64.5,
+      lon: -147.6
+    }, (err, key, node) => console.log(err, key, node));
+    osm.create({
+      id: 'C',
+      lat: 64.5,
+      lon: -147.6
+    }, (err, key, node) => console.log(err, key, node));
+  }
+
+  render() {
+    osm.query([
+      [61, 65],
+      [-149, -147]
+    ], (err, pts) => console.log(err, pts));
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Surveyor', () => Surveyor);
